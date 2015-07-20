39aa10c10c4b70b1ad165e3d022c5414bfa5d422
Dumitru Cantea <canteadumitru@gmail.com>
2015-07-20 10:11:04 +0300

    s3 hash key prefix, test

---
+++ test/client-test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var assert = require('assert');
+var images = require('../lib');
+var client = images.instance();
+
+describe('Client', function() {
+	describe('formatKey', function() {
+		it('ok', function() {
+			assert.equal('323f/a/323f4f34f34f35.jpg', client.formatKey('a', '323f4f34f34f35'));
+		});
+	});
+});
